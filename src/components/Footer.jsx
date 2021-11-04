d29fc852f5f971b8adeebbc0d6c90108ac5eae06
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2021-11-04 17:08:03 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,65 @@
-import styled from "styled-components";
+import {
+  footer,
+  footer_nav,
+  connect_us,
+  copyright,
+} from "../styles/footer.module.scss";
+
+import { footerNav, footerSocialLinks } from "../data";
+import FooterNavItems from "./FooterNavItems";
 
 export default function Footer() {
   return (
-    <Para>
-      🙂 The Project is developed by Muneeb Ali to showcase his React.js skills
-      🙂
-    </Para>
+    <footer className={footer}>
+      {/*//////////////////////// Footer Nav Items */}
+      <nav className={footer_nav}>
+        {footerNav.map((navItem) => (
+          <FooterNavItems
+            title={navItem.title}
+            links={navItem.links}
+            key={navItem.id}
+          />
+        ))}
+      </nav>
+
+      {/*//////////////////////// Footer Socail Links */}
+      <section className={connect_us}>
+        <ul className="follow-us">
+          <li>Follow us</li>
+          <li>
+            {footerSocialLinks.map((link) => (
+              <a
+                key={link.id}
+                href={link.website}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className={link.icon}></i>
+              </a>
+            ))}
+          </li>
+        </ul>
+
+        <ul className="mobile-app">
+          <li>Mobile App</li>
+          <li>
+            <a href="https://www.apple.com/" target="_blank" rel="noreferrer">
+              <i className="fab fa-apple"></i>
+            </a>
+            <a href="https://www.android.com/" target="_blank" rel="noreferrer">
+              <i className="fab fa-android"></i>
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      {/*//////////////////////// Footer Copyright */}
+      <div className={copyright}>
+        <span>© 2015-2021 Upwork* Global Inc.</span>
+        <span>Terms of Service</span>
+        <span>Privacy Policy</span>
+        <span>Accessibility</span>
+      </div>
+    </footer>
   );
 }
-
-const Para = styled.footer`
-  font-size: 1.5rem;
-  color: #fff;
-  background-color: #0236b9;
-  padding: 1.5rem;
-  text-align: center;
-`;
